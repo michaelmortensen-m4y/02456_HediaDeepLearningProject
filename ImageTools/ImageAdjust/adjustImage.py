67852978e0414ce59d4b39f3f5dfe96f21ddd38c
Huayu <s162077@student.dtu.dk>
2018-11-03 17:55:18 +0100

    augmentImage function with pro
    call each adjust method through one function

--- ImageTools/ImageAdjust/adjustImage.py
+++ ImageTools/ImageAdjust/adjustImage.py
@@ -7,7 +7,10 @@ Created on Mon Oct 29 15:14:31 2018
 """
 
 from PIL import Image
+from PIL import ImageFilter
+from skimage import exposure
 import os
+import cv2
 
 def createFolder(directory):
     try:
@@ -17,42 +20,84 @@ def createFolder(directory):
         print ('Error: Creating directory. ' +  directory)
         
  
-def flip_image(image_path, saved_location):
-    """
-    Flip or mirror the image
-    @param image_path: The path to the image to edit
-    @param saved_location: Path to save the cropped image
-    """
-    #path_to_Flipped = (Path.home()  / 'DTU' / 'DeepLearning' /'Hedia'/'food images'/'google'/'291018_1418'/'raw'/'0'/'Flipped')
+def flip(image_path):
+    saved_location = ''.join([str(image_path), 'flip.jpg'])
     image_obj = Image.open(image_path)
-    rotated_image = image_obj.transpose(Image.FLIP_LEFT_RIGHT)
-    rotated_image.save(saved_location)
-    #rotated_image.show()
-    #rotated_image = image_obj.rotate(90)
-    #rotated_image.save(saved_location)
+    fliped_image = image_obj.transpose(Image.FLIP_LEFT_RIGHT)
+    #flip_image.save(saved_location)
+    return fliped_image,saved_location
+  
 
-
-def rotate(image_path, saved_location):
-    """
-    Rotate the given photo the amount of given degreesk, show it and save it
-    @param image_path: The path to the image to edit
-    @param degrees_to_rotate: The number of degrees to rotate the image
-    @param saved_location: Path to save the cropped image
-    """
+def rotate(image_path):
+    saved_location = ''.join([str(image_path), 'rotate.jpg'])
     image_obj = Image.open(image_path)
     rotated_image = image_obj.rotate(45)
-    rotated_image.save(saved_location)
+    #rotated_image.save(saved_location)
+    return rotated_image, saved_location
+    
+def overExposure (image_path):
+    saved_location = ''.join([str(image_path), 'overExp.jpg'])
+    image_obj = Image.open(image_path)
+    im = cv2.imread(image_path)
+    imgRGB = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    #corrected_image = exposure.rescale_intensity(image_path, in_range='image', out_range='dtype')
+    gamma_corrected = exposure.adjust_gamma(imgRGB, 4)
+    img = Image.fromarray(gamma_corrected)
+    img.save(saved_location)
+    return img, saved_location
+
+def underExposure (image_path):
+    saved_location = ''.join([str(image_path), 'underExp.jpg'])
+    image_obj = Image.open(image_path)
+    im = cv2.imread(image_path)
+    imgRGB = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    gamma_corrected = exposure.adjust_gamma(imgRGB, 0.25)
+    img = Image.fromarray(gamma_corrected)
+    img.save(saved_location)
+    return img, saved_location
+
+def blur(image_path):
+    saved_location = ''.join([str(image_path), 'blur.jpg'])
+    image_obj = Image.open(image_path)
+    im1 = image_obj.filter(ImageFilter.GaussianBlur(5))
+    #im1.save(saved_location)
+    return im1, saved_location
+    
+def augmentImage(image_path,proDict):
+    augImage = []
+    augImagePath = []
+    if proDict["flip"] > 0.5:
+        Image, Path = flip(image_path)
+        augImage.append(Image)
+        augImagePath.append(Path)
+    if proDict["rotate"] > 0.5:
+        Image, Path = rotate(image_path)
+        augImage.append(Image)
+        augImagePath.append(Path)
+    if proDict["overExposure"] > 0.5:
+        Image, Path = overExposure(image_path)
+        augImage.append(Image)
+        augImagePath.append(Path)
+    if proDict["underExposure"] > 0.5:
+        Image, Path = underExposure(image_path)
+        augImage.append(Image)
+        augImagePath.append(Path)
+    if proDict["blur"] > 0.5:
+        Image, Path = blur(image_path)
+        augImage.append(Image)
+        augImagePath.append(Path)
+    
+    return augImage, augImagePath
     
     
-  
- 
     
-# =============================================================================
-# if __name__ == '__main__':
-#     image = '375. beef.jpg'
-#     path_to_Adjusted = (Path.home()  / 'DTU' / 'DeepLearning' /'Hedia'/'food images'/'google'/'291018_1418'/'raw'/'0'/'Flipped')
-#     createFolder(path_to_Adjusted)
-#     flip_image(image, path_to_Adjusted/'375. beef.jpg')
-# =============================================================================
-    
+if __name__ == '__main__':
+    image = '375. beef.jpg'
+    pro = {"flip":0.6,
+           "rotate": 0.4,
+           "overExposure": 0.7,
+           "underExposure": 0.8,
+           "blur":0.3
+            }
+    augmentImage(image,pro)
     
